037071b9d034dcc00d5d5a075a6cd4e5395c37fa
sumitkshaw <shawsumit6286@gmail.com>
2025-04-17 17:39:46 +0530

    Update: changes check

--- src/main/java/com/techeazy/aws/batch2/service/S3Service.java
+++ src/main/java/com/techeazy/aws/batch2/service/S3Service.java
@@ -1,8 +1,6 @@
 package com.techeazy.aws.batch2.service;
 
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,14 +9,11 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.S3Object;
 
 @Service
 public class S3Service {
-
 	@Value("${aws.access-key-id}")
 	private String accessKeyId;
 
@@ -31,20 +26,17 @@ public class S3Service {
 	@Value("${aws.s3.bucket-name}")
 	private String bucketName;
 
+
+
 	public void uploadToS3(String localFilePath, String key) {
 
 		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
 
-		S3Client s3 = S3Client.builder()
-				.region(Region.of(region))
-				.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-				.build();
+		S3Client s3 = S3Client.builder().region(Region.of(region)) // change region if needed
+				.credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
 
 		try {
-			PutObjectRequest putOb = PutObjectRequest.builder()
-					.bucket(bucketName)
-					.key(key)
-					.build();
+			PutObjectRequest putOb = PutObjectRequest.builder().bucket(bucketName).key(key).build();
 
 			s3.putObject(putOb, Paths.get(localFilePath));
 			System.out.println(" File uploaded successfully!");
@@ -54,37 +46,5 @@ public class S3Service {
 		}
 
 		s3.close();
-	}
-
-	public List<String> listFilesForUser(String userName) {
-
-		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
-
-		S3Client s3 = S3Client.builder()
-				.region(Region.of(region))
-				.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-				.build();
-
-		List<String> fileNames = null;
-
-		try {
-			ListObjectsV2Request request = ListObjectsV2Request.builder()
-					.bucket(bucketName)
-					.prefix(userName + "/")
-					.build();
-
-			fileNames = s3.listObjectsV2(request)
-					.contents()
-					.stream()
-					.map(S3Object::key)
-					.collect(Collectors.toList());
-
-		} catch (S3Exception e) {
-			System.err.println("List failed: " + e.awsErrorDetails().errorMessage());
-		}
-
-		s3.close();
-
-		return fileNames;
 	}
 }
